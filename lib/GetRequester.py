a842af4e25bc9b34572f5bd7c4a813d7966c7362
Matilda <ogarimatilda@gmail.com>
2024-06-29 12:01:12 +0300

    completed

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -6,8 +6,12 @@ class GetRequester:
     def __init__(self, url):
         self.url = url
 
+    #sends a get request to the url passed on initialization, returns the body of the response
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
+    # sends a request, then returns a dict made up of data converted from the response of that request
     def load_json(self):
-        pass
+        response = requests.get(self.url)
+        return json.loads(response.content)
